5e2d7d13110ecc27c3f3e7fa64812785d4b539b5
Kirishikesan Kannan <kirishikesan.17@cse.mrt.ac.lk>
2021-10-26 12:14:35 +0530

    Added User

--- votingCenter/src/main/java/com/crypto/votingCenter/user/User.java
+++ votingCenter/src/main/java/com/crypto/votingCenter/user/User.java
@@ -8,77 +8,44 @@ import javax.persistence.Id;
 public class User {
 	
 	@Id
-	private String id;
-	@Column(nullable = false)
-	private String name;
-	@Column(nullable = false,unique=true)
-	private String deviceID;
-	@Column(unique=true)
 	private String randomID;
-	@Column(nullable = false,unique=true)
+	@Column(unique=true,nullable = false)
+	private String deviceID;
+	@Column(unique=true,nullable=false)
 	private String email;
-	
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public User() {
-		
-	}
-	
-	public User(String id, String name, String deviceID, String randomID) {
+	@Column(unique=true)
+	private String ballotID;
+	public User(String randomID, String deviceID, String email, String ballotID) {
 		super();
-		this.id = id;
-		this.name = name;
-		this.deviceID = deviceID;
 		this.randomID = randomID;
+		this.deviceID = deviceID;
+		this.email = email;
+		this.ballotID = ballotID;
 	}
-
-
-	public String getID() {
-		return id;
-	}
-
-	public void setID(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getRandomID() {
 		return randomID;
 	}
-
 	public void setRandomID(String randomID) {
 		this.randomID = randomID;
 	}
-
 	public String getDeviceID() {
 		return deviceID;
 	}
-
 	public void setDeviceID(String deviceID) {
 		this.deviceID = deviceID;
 	}
-	
-	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBallotID() {
+		return ballotID;
+	}
+	public void setBallotID(String ballotID) {
+		this.ballotID = ballotID;
+	}
+		
 }
 	
